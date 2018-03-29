36259080af9b291f762d774241ccbbe7f93f2e6c
unknown <tberry6@gmail.com>
2018-03-29 16:28:39 -0700

    Added function to add a card based on the current image to the list (and delete first card). Also now handles urls or image files.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,7 @@ var apikey = "mTG1xZDZC7R-gIdefVSwhaixToHrJd8z";
 var apisecret = "yrgYIWFd5OvheUzrAfiLff0oS9_4XkWF";
 var imgUrl;
 var image;
+var enteredUrl;
 
 function encodeImageFileAsURL(element) {
     var file = element.files[0];
@@ -12,14 +13,50 @@ function encodeImageFileAsURL(element) {
     reader.readAsDataURL(file);
 }
 
-$("#submit").on("click", function () {
-    imgUrl = $("#url").val();
-    var form = new FormData($("form")[0]);
+function makeCard(name, image, tags) {
+    var card = $("<li>");
+    var header = $("<div>");
+    header.addClass("collapsible-header");
+    header.html(`<i class="material-icons">picture_in_picture_alt</i>${name}</div>`);
+    var body = $("<div>");
+    body.addClass("collapsible-body");
 
-    $("#image").attr("src", image);
-    form.append("image_url", imgUrl);
+    var span = $("<span>");
+    var img = $("<img>");
+    img.attr("src", image);
+    var title = $("<span>");
+    title.addClass("card-title");
+    for(var i = 0; i < tags.length; i++) {
+        title.append(`<div class="chip">#${tags[i]}</div>`);
+    }
+    span.append(img, title);
+    body.append(span);
+
+    card.append(header, body);
+    if($(".collapsible").children().length > 3) {
+        $(".collapsible").children().first().remove();
+    }
+    $(".collapsible").append(card);
+}
+
+$("#submit").on("click", function () {
+    var form;
+    imgUrl = $("#url").val();
+    if(imgUrl === "") enteredUrl = false;
+    else enteredUrl = true;
+    if(!enteredUrl) {
+        form = new FormData($("form")[0]);
+        $("#image").attr("src", image);
+    }
+    else {
+        form = new FormData();
+        form.append("image_url", imgUrl);
+        $("#image").attr("src", imgUrl);
+    }
+    
     form.append("api_key", "mTG1xZDZC7R-gIdefVSwhaixToHrJd8z");
     form.append("api_secret", "yrgYIWFd5OvheUzrAfiLff0oS9_4XkWF");
+    form.append("image_url", image);
     form.append("return_attributes", "emotion");
 
     var settings = {
@@ -39,7 +76,12 @@ $("#submit").on("click", function () {
             $(`span.${emotion}`).text(`${emotions[emotion]}%`);
             $(`div.${emotion}`).attr("style", `width: ${emotions[emotion]}%`);
         }
+        if(enteredUrl) makeCard("Name", imgUrl, ["happy", "sad"]);
+        else makeCard("Name", image, ["happy", "sad"]);
     });
+
+    $("#url").val("");
+    $("#file").val("");
 });
 
 $(document).ready(function(){
